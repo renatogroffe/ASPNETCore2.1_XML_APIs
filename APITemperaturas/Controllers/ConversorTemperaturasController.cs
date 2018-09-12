a55273bc5107fb243b0e42c1f64efe56e77ed311
Renato Groffe <renato.groffe@yahoo.com.br>
2018-09-12 15:52:35 -0300

    ASP.NET Core 2.1 + Retornando XML em APIs REST
    ASP.NET Core 2.1 + Retornando XML em APIs REST

---
+++ APITemperaturas/Controllers/ConversorTemperaturasController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APITemperaturas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConversorTemperaturasController : ControllerBase
+    {
+        [HttpGet("Fahrenheit/{temperatura}")]
+        public object GetConversaoFahrenheit(double temperatura)
+        {
+            Temperatura dados = new Temperatura();
+            dados.ValorFahrenheit = temperatura;
+            dados.ValorCelsius =
+                Math.Round((temperatura - 32.0) / 1.8, 2);
+            dados.ValorKelvin = dados.ValorCelsius + 273.15;
+
+            return dados;
+        }
+    }
+}
